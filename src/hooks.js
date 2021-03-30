295a6d861d43464cb9636f1eb645aaa905495c33
dackmin <ugo@poool.fr>
2021-03-30 16:57:22 +0200

    fix: allow custom global for usePoool hook

--- src/hooks.js
+++ src/hooks.js
@@ -2,17 +2,20 @@ import { useContext } from 'react';
 
 import { DefaultContext } from './contexts';
 
-export const usePoool = () => {
+export const usePoool = ({
+  win = typeof window !== 'undefined'
+    ? window : /* istanbul ignore next: ssr edge case */ global,
+} = {}) => {
   const {
     appId, lib, config = {}, styles = {}, texts = {},
   } = useContext(DefaultContext);
 
-  if (!lib || !global.poool) {
-    global.poool = global.poool || function () {
-      global.poool._q = global.poool._q || [];
-      global.poool.push(arguments);
+  if (win && (!lib || !win.poool)) {
+    win.poool = win.poool || function () {
+      win.poool._q = win.poool._q || [];
+      win.poool._q.push(arguments);
     };
   }
 
-  return { poool: lib || global.poool, appId, config, styles, texts };
+  return { poool: lib || win.poool, appId, config, styles, texts };
 };
