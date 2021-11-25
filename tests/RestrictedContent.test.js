224b7adf41de606713315f9c400e48a23f479ee8
dackmin <ugo@poool.fr>
2021-11-25 17:50:33 +0100

    tests: update tests and remove useless ones

--- tests/RestrictedContent.test.js
+++ tests/RestrictedContent.test.js
@@ -1,9 +1,6 @@
-import React, { useContext, forwardRef } from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
-import { DefaultContext } from '../src/contexts';
 import { RestrictedContent } from '../src';
-import PaywallContext from '../src/PaywallContext';
 
 describe('<RestrictedContent />', () => {
 
@@ -12,26 +9,6 @@ describe('<RestrictedContent />', () => {
       <RestrictedContent><div className="test" /></RestrictedContent>
     );
     expect(container.querySelectorAll('.test').length).toBe(1);
-  });
-
-  it('should set content element ref inside context on mount', async () => {
-    const Child = forwardRef((props, ref) => {
-      const { container } = useContext(DefaultContext);
-
-      return <div ref={ref} id="context-container-test">{ container }</div>;
-    });
-
-    const { getByText, unmount } = render(
-      <PaywallContext>
-        <RestrictedContent><Child /></RestrictedContent>
-      </PaywallContext>
-    );
-
-    await waitFor(() => {
-      expect(getByText('context-container-test')).toBeTruthy();
-    });
-
-    unmount();
   });
 
 });
