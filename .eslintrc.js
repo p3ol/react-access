11765bffd7dc670b4de0792652712b368e2f4906
Ugo Stephant <ugo@poool.fr>
2024-07-11 14:54:28 +0200

    chore: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,17 @@
 module.exports = {
-  extends: '@poool/eslint-config-react',
+  extends: [
+    '@poool/eslint-config-react',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
   rules: {
     'react/react-in-jsx-scope': 0,
+    '@typescript-eslint/no-explicit-any': 0,
   },
   overrides: [{
-    files: ['src/**/*.test.js'],
+    files: ['src/**/*.test.{ts,tsx}'],
     env: {
       jest: true,
-    },
-  }, {
-    files: ['src/**/*.{ts,tsx}'],
-    extends: ['plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    globals: {
-      JSX: 'readonly',
-      React: 'readonly',
-    },
-    rules: {
-      // function params are considered as unused vars
-      // 'no-unused-vars': 0,
-      '@typescript-eslint/no-explicit-any': 0,
     },
   }],
 };
